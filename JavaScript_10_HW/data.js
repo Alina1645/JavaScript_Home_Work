1a17442990a6857547b491c0b7d64fc2936320f0
Alina <lina.millou@yandex.ru>
2023-03-13 18:44:48 +0300

    Home_work_10

---
+++ JavaScript_10_HW/data.js
@@ -0,0 +1,12 @@
+const dataInfo =  `[
+ 
+{
+  "created_at": "2020-01-05 13:42:26.194739",
+  "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+  "id": "8k8GUH6cTWmhBLdZ3sBAog",
+  "updated_at": "2020-01-05 13:42:26.194739",
+  "url": "https://api.chucknorris.io/jokes/8k8GUH6cTWmhBLdZ3sBAog",
+  "value": "Chuck Norris once beat Ricky Gervais in a British accent impersonation contest."
+}
+
+]`
